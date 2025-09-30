535d194c517f5e3949858a681e3834f9f6467559
Cesar <cerh_dev@outlook.com>
2025-09-30 14:36:53 -0600

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/IP-Address-Tracker",
+  base: "/", // Changed from "/IP-Address-Tracker"
   server: {
     host: true,
     port: 5173,
